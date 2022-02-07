47bee063a5b8ca06a52ce1d3266e2f80302199da
Devin Trowbridge <dtrowbridge@parrylabs.com>
2022-02-07 09:43:12 -0600

    create install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#/bin/sh
+
+ln -sv '~/dotfiles/.bashrc' ~
+ln -sv '~/dotfiles/.bash_profile' ~
+ln -sv '~/dotfiles/.gitconfig' ~
+ln -sv '~/dotfiles/.gitignore' ~
+ln -sv '~/dotfiles/.tmux.conf' ~
+ln -sv '~/dotfiles/.vimrc' ~
