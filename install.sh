be1f69c8bb9c11efd45c99f8fe2602c9bd7d5502
Devin Trowbridge <dtrowbridge@parrylabs.com>
2022-02-09 13:48:23 -0600

    force soft links and fix install paths

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #/bin/sh
 
-ln -sv '~/dotfiles/.bashrc' ~
-ln -sv '~/dotfiles/.bash_profile' ~
-ln -sv '~/dotfiles/.gitconfig' ~
-ln -sv '~/dotfiles/.gitignore' ~
-ln -sv '~/dotfiles/.tmux.conf' ~
-ln -sv '~/dotfiles/.vimrc' ~
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+ln -svf ${BASEDIR}/.bashrc ~/.bashrc
+ln -svf ${BASEDIR}/.bash_profile ~/.bash_profile
+ln -svf ${BASEDIR}/.gitconfig ~/.gitconfig
+ln -svf ${BASEDIR}/.gitignore ~/.gitignore
+ln -svf ${BASEDIR}/.tmux.conf ~/.tmux.conf
+ln -svf ${BASEDIR}/.vimrc ~/.vimrc
+ln -svf ${BASEDIR}/.env ~/.env
+ln -svf ${BASEDIR}/.aliases ~/.aliases
